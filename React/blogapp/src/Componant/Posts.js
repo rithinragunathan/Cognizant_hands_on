daa7cb0080aa743feb8ea07b1592588458c08686
RITHIN R S <rithinragunathan@gmail.com>
2026-07-16 11:22:56 +0530

    Add files via upload

---
+++ React/blogapp/src/Componant/Posts.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => response.json())
+            .then((data) => {
+
+                const posts = data.map(
+                    (item) =>
+                        new Post(
+                            item.id,
+                            item.title,
+                            item.body
+                        )
+                );
+                console.log(data)
+                this.setState({
+                    posts: posts
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+            
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+        console.log(info);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Posts Component</h2>
+
+                {
+                    this.state.posts.map((post) => (
+                        <div key={post.id}>
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                            <hr />
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default Posts;
